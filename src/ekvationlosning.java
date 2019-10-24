3911f0ac881fbe87ffc93bc3bae9871de9192eea
SimonVestman <simonw202@gmail.com>
2019-10-24 13:30:05 +0200

    Nu går det att lösa förstagrads polynomer

--- src/ekvationlosning.java
+++ src/ekvationlosning.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.lang.String;
 
 /*
 Skapat 2019-10-24
@@ -9,19 +10,23 @@ public class ekvationlosning {
     public static void main(String[] args) {
 
         //Är det en linjär ekvation eller en andragrads ekvation
-        String p =  JOptionPane.showInputDialog(null,"Är det" + "\n" +
+        String p = JOptionPane.showInputDialog(null, "Är det" + "\n" +
                 "1. En linjär ekvation" + "\n" +
                 "2. En andragrads ekvation");
         int et = Integer.parseInt(p);
         //lösning till linjär ekvation
-            if ( et = 1) {
-                String K = JOptionPane.showInputDialog(null, "Skriv den linjära ekvationens K värde.");
-                String Y = JOptionPane.showInputDialog(null, "Skriv den linjära ekvationens Y värde.");
-                String M = JOptionPane.showInputDialog(null, "Skriv den linjära ekvationens M värde.");
+        if (et == 1) {
+           String k = JOptionPane.showInputDialog(null,"Skriv värdet på k");
+           String m = JOptionPane.showInputDialog(null,"Skriv värdet på m");
+           double K = Double.parseDouble(k);
+           double M = Double.parseDouble(m);
+                double x;
+                    x= -M/K;
+                    JOptionPane.showMessageDialog(null,"X är = " + x);
 
-            } else if ( et = 2) {
+                } else (et == 2) {
 
-            }
+        }
     }
 }
 
